a16cc3b7a98d2617d35335d7050da70d902308ed
akanshamingwal <50590013+akanshamingwal@users.noreply.github.com>
2020-07-15 23:24:51 +0530

    Add files via upload

---
+++ mod4_solution/script.js
@@ -0,0 +1,13 @@
+
+
+(function() {
+    var names = ["jay","Akansha", "Anshuman", "Jason", "jordan", "Marshall", "Mathers"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
